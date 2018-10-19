05d3c846a51a1f39f074b4a999b94a554aac13ef
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-20 00:54:42 +0900

    메인화면 바텀 네비게이션 바 구현

--- app/src/main/java/apiteam/allpoisonim/features/main/MainActivity.kt
+++ app/src/main/java/apiteam/allpoisonim/features/main/MainActivity.kt
@@ -1,13 +1,41 @@
 package apiteam.allpoisonim.features.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 import apiteam.allpoisonim.R
+import apiteam.allpoisonim.features.books.BooksFragment
+import apiteam.allpoisonim.features.mypage.MyPageFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
     }
+
+    private fun initUi() {
+        val fragments = listOf(HomeFragment(), BooksFragment(), MyPageFragment())
+        vp_main.adapter = MainPagerAdapter(fragments, supportFragmentManager)
+        vp_main.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                bottom_navigation.menu.getItem(position).isChecked = true
+            }
+        })
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_home -> vp_main.currentItem = 0
+                R.id.item_books -> vp_main.currentItem = 1
+                R.id.item_my_page -> vp_main.currentItem = 2
+            }
+            true
+        }
+    }
+
 }
