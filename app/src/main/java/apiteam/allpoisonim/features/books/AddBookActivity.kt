8ac3dc8b646619d40fa7f015473ed6efba46f0c1
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-21 18:23:31 +0900

    서적 추천 등록 화면 View 그림

---
+++ app/src/main/java/apiteam/allpoisonim/features/books/AddBookActivity.kt
@@ -0,0 +1,16 @@
+package apiteam.allpoisonim.features.books
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import apiteam.allpoisonim.R
+import kotlinx.android.synthetic.main.activity_add_book.*
+
+
+class AddBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_book)
+    }
+
+}
--- app/src/main/java/apiteam/allpoisonim/features/books/BooksFragment.kt
+++ app/src/main/java/apiteam/allpoisonim/features/books/BooksFragment.kt
@@ -1,18 +1,32 @@
 package apiteam.allpoisonim.features.books
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import apiteam.allpoisonim.R
+
+import kotlinx.android.synthetic.main.fragment_books.*
 
 class BooksFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_books, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
+        btn_add_book.setOnClickListener {
+            val intent = Intent(context, AddBookActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
