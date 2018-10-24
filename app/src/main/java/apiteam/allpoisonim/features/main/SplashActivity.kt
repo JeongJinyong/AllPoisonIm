3945767f1b7d07a298ce08f1fb5d5d1ba65a6e89
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-25 00:29:24 +0900

    Splash 화면 추가

---
+++ app/src/main/java/apiteam/allpoisonim/features/main/SplashActivity.kt
@@ -0,0 +1,36 @@
+package apiteam.allpoisonim.features.main
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import apiteam.allpoisonim.R
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        initUi()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
+    private fun initUi() {
+        compositeDisposable.add(Observable.timer(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                })
+    }
+}
