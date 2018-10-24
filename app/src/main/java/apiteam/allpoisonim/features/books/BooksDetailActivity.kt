1d50305603bd0198872b4ef573473dc95c3ac95b
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-24 20:26:57 +0900

    [서적 추천 상세 화면] 댓글 화면 추가

--- app/src/main/java/apiteam/allpoisonim/features/books/BooksDetailActivity.kt
+++ app/src/main/java/apiteam/allpoisonim/features/books/BooksDetailActivity.kt
@@ -3,11 +3,19 @@ package apiteam.allpoisonim.features.books
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import apiteam.allpoisonim.R
+import kotlinx.android.synthetic.main.activity_books_detail.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.DividerItemDecoration
+
+
 
 class BooksDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books_detail)
+
+        val replyAdapter = ReplyAdapter(this, listOf(0,1,2,3,4,5))
+        rv_reply.adapter = replyAdapter
     }
 }
