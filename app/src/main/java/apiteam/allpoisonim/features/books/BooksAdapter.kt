ecff0097a31986aeba75fd74d411e8f122570fc3
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-24 14:24:32 +0900

    [서적 추천 추가 화면] 서적 추천 화면 뷰홀더 수정. 상세화면 그리는 중

--- app/src/main/java/apiteam/allpoisonim/features/books/BooksAdapter.kt
+++ app/src/main/java/apiteam/allpoisonim/features/books/BooksAdapter.kt
@@ -1,15 +1,21 @@
 package apiteam.allpoisonim.features.books
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import apiteam.allpoisonim.R
+import kotlinx.android.synthetic.main.viewholder_book.view.*
 
 class BooksAdapter(val context: Context, var bookList: List<Int>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.viewholder_book, parent, false)
+        view.setOnClickListener {
+            val intent = Intent(context, BooksDetailActivity::class.java)
+            context.startActivity(intent)
+        }
         return BookViewHolder(view)
     }
 
@@ -25,9 +31,25 @@ class BooksAdapter(val context: Context, var bookList: List<Int>) : RecyclerView
 
     // ViewHolder Class
     class BookViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        private val ivProfile = v.iv_profile
+        private val tvName = v.tv_title
+        private val tvCreatedAt = v.tv_created_at
+        private val tvDesc = v.tv_desc
+        private val ivThumbnail = v.iv_thumbnail
+        private val tvBookName = v.tv_book_name
+        private val tvBookInfo = v.tv_book_info
+        private val tvPublishedAt = v.tv_book_info2
+        private val btnScrap = v.btn_scrap
+        private val btnReply = v.btn_reply
 
         fun bind(number: Int) {
-
+            // 프로필 사진 로딩
+            // 사용자 이름 설정
+            // 만든 시간 ~초전 설정
+            // 탐라 내용 설정
+            // 책 썸네일, 이름, 책정보 설정
+            // 추천 버튼 설정
+            // 댓글 버튼 설정
         }
     }
 }
---
+++ app/src/main/java/apiteam/allpoisonim/features/books/BooksDetailActivity.kt
@@ -0,0 +1,13 @@
+package apiteam.allpoisonim.features.books
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import apiteam.allpoisonim.R
+
+class BooksDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_detail)
+    }
+}
