9102ca269f7d3c72104b6b219edd218d08f5811a
jeongjinyoung <image5956@cyworld.biz>
2018-10-26 15:48:16 +0900

    서버 요청 API 변경

--- app/src/main/java/apiteam/allpoisonim/api/HttpRequest.kt
+++ app/src/main/java/apiteam/allpoisonim/api/HttpRequest.kt
@@ -3,32 +3,59 @@ package apiteam.allpoisonim.api
 import apiteam.allpoisonim.api.data.BookStore
 import apiteam.allpoisonim.api.data.User
 import io.reactivex.Single
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
+import java.io.IOException
 
 interface HttpRequest {
 
     companion object {
         fun create(): HttpRequest {
+            val httpLogging = HttpLoggingInterceptor()
+            httpLogging.level = HttpLoggingInterceptor.Level.BODY
+            val okHttpClient = OkHttpClient().newBuilder().apply {
+                addInterceptor(HeaderSettingInterceptor())
+                addInterceptor(httpLogging)
+
+            }.build()
             val retrofit = Retrofit.Builder()
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl("http://13.209.168.105:7777")
-                    .build()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://13.209.168.105:7777")
+                .client(okHttpClient)
+                .build()
 
             return retrofit.create(HttpRequest::class.java)
         }
     }
 
+    private class HeaderSettingInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+
+            val chainRequest = chain.request()
+
+            val request = chainRequest.newBuilder().apply {
+                header("Accept", "application/json")
+                header("Content-Type", "application/json")
+            }.build()
+
+            return chain.proceed(request)
+        }
+    }
+
     @POST("/user/sign-up")
-    @FormUrlEncoded
-    fun signUp(@FieldMap map: Map<String, String>): Single<User.Sign>
+    fun signUp(@Body map: Map<String, String>): Single<User.Sign>
 
     @POST("/user/sign-in")
-    @FormUrlEncoded
-    fun signIn(@FieldMap map: Map<String, String>): Single<User.Sign>
+    fun signIn(@Body map: Map<String, String>): Single<User.Sign>
 
     @GET("/book-store/{id}")
     fun bookStore(@Path("od") id: String): Single<BookStore.Data>
