ad625b4625d034a86227b130adae1cb30e837e2b
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-10-26 16:58:53 +0900

    서점 상세화면 첫번째 뷰홀더 연동 완료

--- app/src/main/java/apiteam/allpoisonim/features/main/bookstoreviewholder/BookStoreType1.kt
+++ app/src/main/java/apiteam/allpoisonim/features/main/bookstoreviewholder/BookStoreType1.kt
@@ -2,12 +2,26 @@ package apiteam.allpoisonim.features.main.bookstoreviewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import apiteam.allpoisonim.R
 import apiteam.allpoisonim.api.data.BookStore
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.viewholder_bookstore_type_1.view.*
+import java.lang.Exception
 
 class BookStoreType1(val view: View) : RecyclerView.ViewHolder(view) {
+    private val ivMark = view.bookstore_name_img
+    private val tvVol = view.bookstore_vol_txt
 
     fun bind(store: BookStore.Data) {
-//        bookstore_vol_txt.text = "vol. " + if(bookstore.id.toString().length == 1) "0${bookstore.id}" else bookstore.id.toString()
-//        Glide.with(context).load(bookstore.storeMainImage).into(bookstore_name_img)
+        tvVol.text = String.format("vol. %s", store.id)
+        Picasso.get().load(store.storeMainImage).into(ivMark, object : Callback {
+            override fun onSuccess() {
+            }
+
+            override fun onError(e: Exception?) {
+                ivMark.setImageResource(R.drawable.bookscore_bi)
+            }
+        })
     }
 }
